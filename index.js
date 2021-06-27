a765070bd17b13a90073e049c2c546b74cf86779
Charles <Charles@0x7f.cc>
2021-06-28 03:26:06 +0800

    Fix bug of getphoto

--- index.js
+++ index.js
@@ -66,11 +66,12 @@ app.post(
 );
 
 app.get("/photo/:uuid", function (req, res) {
-  var image = storage.getFile(req.params.uuid).toString();
+  var image = storage.getFile(req.params.uuid);
   if (!image) {
     res.sendStatus(404);
     return true;
   }
+  image = image.toString();
   image = image.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
   res.setHeader("Content-Type", image[1]);
   res.send(Buffer.from(image[2], "base64"));
